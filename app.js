8354f3d4893d50728b85a4cb15f89f479f625fa2
Tzu-Hao Huang <ryu1031@gmail.com>
2016-01-24 15:44:46 -0500

    minor change for initiattion

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+'use strict';
+
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3001;
 
 app.get('/', function(request, response){
   response.send('OK\n');
 });
 
-app.listen(3001, function(){
+
+app.listen(port, function(){
   console.log('Listening on port 3001');
 });
